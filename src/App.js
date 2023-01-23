71d183b8cdd751538e2b96e4bb587c39f0765ec3
biethereos <nguyenthanhhung@gmail.com>
2023-01-23 18:08:14 +0700

    first Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/MenuComp';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-expand-sm bg-dark navbar-light">
+      <div>
+        <nav className="navbar navbar-expand-sm bg-primary navbar-light">
           <a className="navbar-brand" href="/">
-            <img src="https://i.imgur.com/C8Mby7w.png" alt="Logo" width={40} />
+            <img src="https://i.imgur.com/Y3Tr8ZQ.png" alt="Logo" width={40} />
           </a>
         </nav>
+        <Menu />
       </div>
     );
   }
