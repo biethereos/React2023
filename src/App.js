22f973b8b2c412a9a254f5410eaf5e081dc65bf1
biethereos <nguyenthanhhung@gmail.com>
2023-01-23 16:16:42 +0700

    updated Logo Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-expand-sm bg-dark navbar-light">
+          <a className="navbar-brand" href="/">
+            <img src="https://i.imgur.com/C8Mby7w.png" alt="Logo" width={40} />
+          </a>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
